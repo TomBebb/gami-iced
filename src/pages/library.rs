6279b691d5f58993a2f11da339f2933fa5a14388
Tom D Bebbington <tombebb@protonmail.com>
2024-12-11 20:40:58 +0000

    Dynamic load game actions

--- src/pages/library.rs
+++ src/pages/library.rs
@@ -1,8 +1,10 @@
+use iced::advanced::svg::Handle;
 use iced::alignment::Vertical;
-use iced::widget::{button, column, combo_box, row, scrollable, svg, text, Svg};
-use iced::{ContentFit, Element};
+use iced::widget::{button, column, combo_box, row, scrollable, svg, text, Container, Svg};
+use iced::{ContentFit, Element, Fill, Theme};
 use iced_aw::ContextMenu;
 use std::fmt;
+
 #[derive(Clone, Debug, Copy)]
 pub enum InstallStatus {
     Installed,
@@ -59,36 +61,80 @@ impl Default for LibraryPage {
 pub enum Message {
     ViewSelected(LibraryViewType),
     ShowAddDialog,
-    PlayGame(LibraryGame),
-    InstallGame(LibraryGame),
+    GameAction(GameActionKind, LibraryGame),
 }
-
+#[derive(Debug, Clone, Copy)]
+enum GameActionKind {
+    Play,
+    Install,
+    Uninstall,
+    Delete,
+    Edit,
+}
+#[derive(Debug, Clone, Copy)]
+struct GameAction {
+    name: &'static str,
+    icon: &'static [u8],
+    kind: GameActionKind,
+}
+const PLAY_ACTION: GameAction = GameAction {
+    name: "Play",
+    icon: include_bytes!("../icons/tabler--play.svg"),
+    kind: GameActionKind::Play,
+};
+const INSTALL_ACTION: GameAction = GameAction {
+    name: "Install",
+    icon: include_bytes!("../icons/tabler--plus.svg"),
+    kind: GameActionKind::Install,
+};
+const UNINSTALL_ACTION: GameAction = GameAction {
+    name: "Uninstall",
+    icon: include_bytes!("../icons/tabler--minus.svg"),
+    kind: GameActionKind::Uninstall,
+};
+const DELETE_ACTION: GameAction = GameAction {
+    name: "Delete",
+    icon: include_bytes!("../icons/tabler--x.svg"),
+    kind: GameActionKind::Delete,
+};
+const EDIT_ACTION: GameAction = GameAction {
+    name: "Edit",
+    icon: include_bytes!("../icons/tabler--edit.svg"),
+    kind: GameActionKind::Edit,
+};
+const fn get_actions(status: InstallStatus) -> &'static [GameAction] {
+    match status {
+        InstallStatus::Installed => &[PLAY_ACTION, EDIT_ACTION, DELETE_ACTION],
+        _ => &[INSTALL_ACTION, EDIT_ACTION, DELETE_ACTION],
+    }
+}
 impl LibraryPage {
     fn game_menu<'a>(
         &'a self,
         game: &'a LibraryGame,
         underlay: Element<'a, Message>,
     ) -> Element<'a, Message> {
+        let actions = get_actions(game.install_status);
         ContextMenu::new(underlay, || {
-            column(vec![
-                iced::widget::button("Play")
-                    .on_press_with(|| Message::PlayGame(game.clone()))
-                    .into(),
-                iced::widget::button("Install")
-                    .on_press_with(|| Message::InstallGame(game.clone()))
-                    .into(),
-            ])
+            column(actions.iter().map(|ga| {
+                let svg: Svg<'static, Theme> = Svg::new(Handle::from_memory(ga.icon));
+                button(row![svg.width(24), text(ga.name)])
+                    .on_press_with(|| Message::GameAction(ga.kind, game.clone()))
+                    .width(Fill)
+                    .into()
+            }))
+            .width(100)
             .into()
         })
         .into()
     }
     pub fn view(&self) -> Element<Message> {
-        let mut items: Vec<Element<Message>> = Vec::new();
-        for game in &self.games {
-            let raw = text(&game.name);
-
-            items.push(self.game_menu(game, raw.into()).into());
-        }
+        let items: Vec<Element<Message>> = self
+            .games
+            .iter()
+            .map(|game| (game, Element::from(row![text(&game.name)].width(Fill))))
+            .map(|(game, raw)| self.game_menu(game, Container::new(raw).into()))
+            .collect();
 
         let toolbar = Element::from(
             row![
