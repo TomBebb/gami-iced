9bc2c77d2c5aa6d5e2232136babc1a42f8743c9e
Tom D Bebbington <tombebb@protonmail.com>
2024-12-11 17:16:37 +0000

    Disable nav btns

--- src/widgets/nav_view.rs
+++ src/widgets/nav_view.rs
@@ -1,28 +1,28 @@
-use iced::widget::{button, column, Column};
-use iced::Fill;
+use iced::widget::{button, Column};
+use iced::{Element, Fill};
 
 #[derive(Copy, Clone, Debug)]
 pub enum Message {
     NavSelected(usize),
 }
-
+const PAGE_NAMES: &'static [&'static str] = &["Counter", "Library", "Settings"];
 #[derive(Debug, Clone, Copy, Default)]
 pub struct NavView {
     pub active_item: usize,
 }
 impl NavView {
     pub fn view(&self) -> Column<Message> {
-        column![
-            button("Counter demo")
-                .on_press(Message::NavSelected(0))
-                .width(Fill),
-            button("Library")
-                .on_press(Message::NavSelected(1))
-                .width(Fill),
-            button("Settings")
-                .on_press(Message::NavSelected(2))
-                .width(Fill),
-        ]
+        Column::with_children(PAGE_NAMES.into_iter().enumerate().map(|(index, &name)| {
+            Element::from(
+                button(name)
+                    .width(Fill)
+                    .on_press_maybe(if self.active_item == index {
+                        None
+                    } else {
+                        Some(Message::NavSelected(index))
+                    }),
+            )
+        }))
         .width(100)
     }
     pub fn update(&mut self, message: Message) {
