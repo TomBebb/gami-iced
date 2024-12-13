18ff801c82e4eda203bc4d0a75917e914d0be19e
Tom D Bebbington <tombebb@protonmail.com>
2024-12-13 15:33:00 +0000

    Remove redundant rust code

--- addon-steam/src/index.ts
+++ addon-steam/src/index.ts
@@ -1,4 +1,4 @@
-import type {GameLibraryRef} from "@gami/sdk";
+import {GameInstallStatus, GameLibraryRef, openUrl, registerAddon, ScannedGameLibraryMetadata} from "@gami/sdk";
 import {parse} from "@node-steam/vdf";
 import {readdir, readFile} from "fs/promises";
 import * as path from "node:path";
@@ -27,24 +27,18 @@ interface AppState {
 
 type GameKv = { AppState: AppState };
 
-async function readKv(path: string): Promise<GameKv> {
-
+async function readSteamInfo(path: string): Promise<GameKv> {
+    const content = await readFile(path, 'utf-8')
+    return parse(content)
 }
 
 function runSteamCmd(cmd: string, ref: GameLibraryRef,): Promise<void> {
-    // openUrl(`steam://${cmd}/${ref.libraryId}`)
-    return Promise.resolve()
+    openUrl(`steam://${cmd}/${ref.libraryId}`)
+    return Promise.resolve();
 }
 
 const appPath = "/home/tom/.steam/steam/steamapps"
-for (const scanPath of await readdir(appPath)) {
-    if (!scanPath.endsWith(".acf")) continue;
-    const content = await readFile(path.join(appPath, scanPath), 'utf-8')
-    const val = parse(content)
 
-    console.info(val)
-}
-/*
 export default registerAddon({
     type: "library",
     id: "steam",
@@ -53,10 +47,21 @@ export default registerAddon({
     uninstall: runSteamCmd.bind(null, "uninstall"),
 
     launch: runSteamCmd.bind(null, "rungameid"),
-    scan(): Promise<ScannedGameLibraryMetadata[]> {
+    async scan(): Promise<ScannedGameLibraryMetadata[]> {
+        const items: ScannedGameLibraryMetadata[] = [];
+        for (const scanPath of await readdir(appPath)) {
+            if (!scanPath.endsWith(".acf")) continue;
+            const {AppState: app} = await readSteamInfo(path.join(appPath, scanPath));
+            items.push({
+                name: app.name,
+                library_id: app.appid.toString(),
+                library_type: "steam",
+
+            })
+        }
         return Promise.resolve([])
     },
     check_install_status(game: GameLibraryRef): Promise<GameInstallStatus> {
         return Promise.resolve(GameInstallStatus.InLibrary) as Promise<GameInstallStatus>;
     }
-})*/
+})
