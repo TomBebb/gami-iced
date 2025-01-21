008741a8daea04bb832c37942b8c551a406eaf0e
Tom D Bebbington <tombebb@protonmail.com>
2025-01-21 01:19:15 +0000

    Simplify text display for now

--- ui-shared/src/lib.rs
+++ ui-shared/src/lib.rs
@@ -4,14 +4,14 @@ use tl::{NodeHandle, Parser, ParserOptions};
 
 fn from_html<'a, 'b, TMessage>(parser: &'a Parser, node_handle: &'a NodeHandle) -> Element<'b, TMessage> {
     let node = node_handle.get(parser).expect("Invalid HTML parse");
-    match node.as_tag() {
-        Some(v) => text(format!("HTML tag: {}", v.inner_text(parser))).into(),
-        None => text("Missing tag").into(),
-    }
+
+    let raw = node.as_raw();
+    text(format!("Tag: {:?}; raw: {:?}", node.as_tag(), raw)).into()
 }
 pub fn show_html<'a, TMessage: 'a>(html: &'a str) -> Element<'a, TMessage> {
     let dom = tl::parse(html, ParserOptions::default()).unwrap();
     let parser = dom.parser();
+
     let mut items = Column::with_capacity(dom.children().len());
     for child in dom.children() {
         items = items.push(from_html(parser, child));
