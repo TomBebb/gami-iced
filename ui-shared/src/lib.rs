2528937768b11f2f8d808689576d99f35a2eaea8
Tom D Bebbington <tombebb@protonmail.com>
2025-01-21 04:10:44 +0000

    Handle newline

--- ui-shared/src/lib.rs
+++ ui-shared/src/lib.rs
@@ -1,23 +1,29 @@
 use html5gum::{Token, Tokenizer};
-use iced::widget::{text, Column, Text};
-use iced::Element;
-use std::fmt::Write;
+use iced::widget::{text, Column, Row};
+use iced::{Element, Length, Renderer, Theme};
 
-pub fn draw_html<'a, TMessage>(html: &'a str) -> Element<'a, TMessage> {
-    let mut new_html = String::new();
+pub fn draw_html<'a, TMessage: 'a>(html: &'a str) -> Element<'a, TMessage> {
+    let mut col = Column::with_capacity(4);
+    let mut row: Row<'a, TMessage, Theme, Renderer> = Row::with_capacity(4);
     for Ok(token) in Tokenizer::new(html) {
         match token {
-            Token::StartTag(tag) => {
-                write!(new_html, "<{}>", String::from_utf8_lossy(&tag.name)).unwrap();
-            }
-            Token::String(hello_world) => {
-                write!(new_html, "{}", String::from_utf8_lossy(&hello_world)).unwrap();
+            Token::StartTag(tag) => match tag.name.as_ref() {
+                b"br" => {
+                    col = col.push(row.width(Length::FillPortion(1)));
+                    row = Row::with_capacity(4);
+                }
+                _ => row = row.push(text!("<{}>", String::from_utf8_lossy(&tag.name))),
+            },
+            Token::String(raw_text) => {
+                let mapped = String::from_utf8(raw_text.0).unwrap();
+                row = row.push(text(mapped));
             }
             Token::EndTag(tag) => {
-                write!(new_html, "</{}>", String::from_utf8_lossy(&tag.name)).unwrap();
+                row = row.push(text!("</{}>", String::from_utf8_lossy(&tag.name)));
             }
             _ => panic!("unexpected input"),
         }
     }
-    text(new_html).into()
+    col = col.push(row.width(Length::FillPortion(1)));
+    col.into()
 }
