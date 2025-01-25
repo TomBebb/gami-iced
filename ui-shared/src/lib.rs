9dcb562f92911a120be0631375412ef21539c136
Tom D Bebbington <tombebb@protonmail.com>
2025-01-25 01:20:24 +0000

    Basic parent tracking

--- ui-shared/src/lib.rs
+++ ui-shared/src/lib.rs
@@ -5,6 +5,7 @@ use iced::{Element, Length, Renderer, Theme};
 pub fn draw_html<'a, TMessage: 'a>(html: &'a str) -> Element<'a, TMessage> {
     let mut col = Column::with_capacity(4);
     let mut row: Row<'a, TMessage, Theme, Renderer> = Row::with_capacity(4);
+    let mut nested_by_tags = Vec::<Box<str>>::new();
     for Ok(token) in Tokenizer::new(html) {
         match token {
             Token::StartTag(tag) => match tag.name.as_ref() {
@@ -12,14 +13,19 @@ pub fn draw_html<'a, TMessage: 'a>(html: &'a str) -> Element<'a, TMessage> {
                     col = col.push(row.width(Length::FillPortion(1)));
                     row = Row::with_capacity(4);
                 }
-                _ => row = row.push(text!("<{}>", String::from_utf8_lossy(&tag.name))),
+                _ => nested_by_tags
+                    .push(unsafe { std::str::from_utf8_unchecked(&tag.name.0) }.into()),
             },
             Token::String(raw_text) => {
+                let curr_tag = nested_by_tags.last_mut();
+                if let Some(curr_tag) = curr_tag {
+                    println!("Tag: {:?}", curr_tag);
+                }
                 let mapped = String::from_utf8(raw_text.0).unwrap();
                 row = row.push(text(mapped));
             }
             Token::EndTag(tag) => {
-                row = row.push(text!("</{}>", String::from_utf8_lossy(&tag.name)));
+                nested_by_tags.pop();
             }
             _ => panic!("unexpected input"),
         }
